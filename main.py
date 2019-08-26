24b6b50c1ec46c61ca19b1e805a473a31148458c
ddorn <diego.dorn@free.fr>
2019-08-26 17:18:11 +0200

    Sitched to tiles representation

--- main.py
+++ main.py
@@ -3,30 +3,23 @@ Solver for Tetravex.
 
 
 Convention for SAT variables names:
-    Each variable encode a position and a color.
-    Let c be the color number from 0 to c-1 included.
-    Let s be side of the tile as follows:
-        0 => Top
-        1 => Right
-        2 => Bottom
-        3 => Left
+    Each variable encode a position and a tile.
+    Let t be the tile number (between 1 and n*n).
     Let p be the position on the board, increasing right then down.
-    The variable corresponding to "The side s of the tile p has color c"
-    is (4 * p + s)* c + c + 1. Therefore is n is a variable, one can extract
-    p, s and c this way:
-        c = (n - 1) % c
-        s = (n // c - 1) % 4
-        p = (n // c - 1) // 4.
+    The variable corresponding to "The tile t is at p" is p * NTILES + t.
+    Therefore if n is a variable, one can extract t and p.
+        t = n % NTILES
+        p = n // NTILES
 
     Those variables are enough to represent any configuration of a n*n board
-    and to solve the Teravex problem.
+    and to solve the Tetravex problem.
 
 Matinfoly 2019.
 """
 
 
 END = ' 0\n'
-COL = 0
+NTILES = 0
 
 
 def print0(*args, **kwargs):
@@ -35,46 +28,35 @@ def print0(*args, **kwargs):
     print(*args, **kwargs)
 
 
-def dim(p, s, c):
-    """Convert a (Position, Side, Color) triplet to a dimacs variable name."""
-    return (4 * p + s) * COL + c + 1
+def dim(p, t):
+    """Convert a (position, tile) tuple to a dimacs variable name."""
+    return p * NTILES + t
+
 
 def psc(dim):
-    """Convert a variable id to a (Position, Side, Color) triplet."""
-    return ((dim - 1) % COL,
-            (n // COL - 1) % 4,
-            (n // COL - 1) // 4)
+    """Convert a dimacs variable id to a (position, tile) tuple."""
+    return (dim % NTILES, dim // NTILES)
 
 
-def gen_unique_color_on_spot(n, c):
+def gen_unique_tiles_on_spot(n, t):
     """
-    Each quarter of tile on the board must have only one color.
+    Each position must have only one tile.
     This generates the CNF describing this.
     """
 
-    # forall p,s exists c such that x_psc
+    # for all position there is a tile
     for p in range(n*n):
-        for s in range(4):
-            # ps1 or ps2 or ...
-            print0(*(dim(p, s, col) for col in range(c)))
+        # p1 or p2 or ...
+        print0(*(dim(p, ti, t) for ti in range(t)))
 
     # there are not two true at the same time (#uniquness)
     for p in range(n*n):
-        for s in range(4):
-            for c1 in range(c):
-                for c2 in range(c1+1, c):
-                    print0(-dim(p, s, c1), -dim(p, s, c2))
-
-
-def gen_form_tiles(n, c, tiles):
-    ...
+        for t1 in range(t):
+            for t2 in range(t1+1, t):
+                print0(-dim(p, t1), -dim(p, t2))
 
 
 def gen_adjacents(n, c):
-    ...
-
-
-def gen_unique_place(n, c, tiles):
     ...
 
 
@@ -83,27 +65,22 @@ def main():
     Solver for Teraflex.
 
     Input:
-        n: size of the teravex
+        n: size of the tetravex
         c: number of colors
         c1 c2 c3 c4: (n*n lines) top, right, bottom and left side of a tile.
     """
 
-    global COL
-
     # input
 
     n = int(input())
-    COL = c = int(input())
+    c = int(input())
     tiles = [list(map(int, input().split())) for _ in range(n*n)]
 
     # generate DIMACS on stdout
 
-    gen_unique_color_on_spot(n, c)
-    gen_form_tiles(n, c, tiles)
+    print("HEADER")
+    gen_unique_tiles_on_spot(n, c)
     gen_adjacents(n, c)
-    gen_unique_place(n, c, tiles)
-
-
 
 
 if __name__ == '__main__':
