d8474d40cf282018b60fda8cabb453f3a36f71db
ddorn <diego.dorn@free.fr>
2019-08-27 18:15:06 +0200

    added click for command line

---
+++ gen.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+"""
+generateur aléatoire :
+    prendre les conditions et générer une grille complète et résolue
+
+
+By Alexia Gross
+"""
+
+import random
+import click
+
+@click.command()
+@click.argument('height', default=4)
+@click.argument('width', default=4)
+@click.argument('colors', default=4)
+def generate(height, width, colors):
+    """
+    Generate a doable HEIGHTxWIDTH Tetravex puzzle with COLORS colors.
+    """
+
+    assert height > 1
+    assert width > 1
+    assert colors > 1
+
+    size = height * width
+
+    grille=[[0,0,0,0] for t in range (size)]  #4 couleurs pour une tuile
+    c = colors - 1
+    print(height, width)
+    print(colors)
+
+    for i in range (size):
+        colonne = i % width
+        if width - 1 != colonne :
+            grille[i][1] = grille[i+1][3]=random.randint(0, c)
+        else:
+            grille[i][1]= random.randint(0, c)
+        if colonne==0:
+            grille[i][3] = random.randint(0, c)
+
+    for i in range (size):
+        if i < (height - 1) * width:
+            grille[i][2] = grille[i + width][0] = random.randint(0, c)
+        else:
+            grille[i][2]  = random.randint(0, c)
+        if i < width:
+            grille[i][0] = random.randint(0, c)
+
+    random.shuffle(grille)
+
+    for t in grille:
+        print(*t)
+
+
+if __name__ == '__main__':
+    generate()
+
